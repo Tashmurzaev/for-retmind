3e5038b7f85bd9942dfa5e1000236c61133d3f1b
sakydin <sakydin.tashmurzaev.kk@gmail.com>
2022-11-02 20:02:21 +0600

    fixed key error and install uuid

--- src/components/nftImages/NFTImages.js
+++ src/components/nftImages/NFTImages.js
@@ -1,18 +1,19 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import uuid from "react-uuid";
 import Button from "../UI/button/Button";
 import { fetchCustomers } from "../../asyncActions/customers";
 import Loader from "../UI/loader/Loader";
 import Card from "@mui/material/Card";
-
 import CardMedia from "@mui/material/CardMedia";
-
 import { CardActionArea } from "@mui/material";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 const NFTImages = () => {
   const dispatch = useDispatch();
   const customers = useSelector((state) => state.customers.customers);
-
+  console.log(customers);
   useEffect(() => {
     dispatch(fetchCustomers());
   }, []);
@@ -20,22 +21,48 @@ const NFTImages = () => {
   return (
     <div>
       {customers.length > 0 ? (
-        <div>
+        <div
+          style={{
+            width: "1280px",
+            margin: "0 auto",
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "20px",
+          }}
+        >
           {customers.map((customer) => (
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
+            <Card key={uuid()} sx={{ maxWidth: 345 }}>
+              <CardActionArea key={uuid()}>
                 <CardMedia
                   component="img"
-                  height="400"
+                  height="300"
                   image={customer.image_url}
-                  alt="nft image"
+                  alt={customer.name}
+                  key={uuid()}
                 />
               </CardActionArea>
+              <CardContent key={uuid()}>
+                <Typography
+                  key={uuid()}
+                  gutterBottom
+                  variant="h5"
+                  component="div"
+                >
+                  {customer.name}
+                </Typography>
+              </CardContent>
+              <Button
+                fullWidth
+                color="secondary"
+                variant="contained"
+                key={uuid()}
+              >
+                Подробнее
+              </Button>
             </Card>
           ))}
         </div>
       ) : (
-        
         <Loader />
       )}
     </div>
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -6,15 +6,6 @@ export const customerReducer = (state = defaultState, action) => {
   switch (action.type) {
     case "ADD_MANY_CUSTOMERS":
       return { ...state, customers: [...state.customers, ...action.payload] };
-    case "ADD_CUSTOMER":
-      return { ...state, customers: [...state.customers, action.payload] };
-    case "REMOVE_CUSTOMERS":
-      return {
-        ...state,
-        customers: state.customers.filter(
-          (customer) => customer.id !== action.payload
-        ),
-      };
     default:
       return state;
   }
